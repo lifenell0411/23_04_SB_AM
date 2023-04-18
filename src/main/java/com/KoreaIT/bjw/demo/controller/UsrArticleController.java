44d877b5a1f8e74f9563da52851ad4c157a39ae8
lifenell0411 <lifenell0411@gmail.com>
2023-04-18 17:18:31 +0900

    로그아웃 기능 구현

--- src/main/java/com/KoreaIT/bjw/demo/controller/UsrArticleController.java
+++ src/main/java/com/KoreaIT/bjw/demo/controller/UsrArticleController.java
@@ -65,7 +65,8 @@ public class UsrArticleController {
 
 		Article article = articleService.getArticle(id);
 
-		return ResultData.from(writeArticleRd.getResultCode(), writeArticleRd.getMsg(), article);
+	
+		return ResultData.newData(writeArticleRd, article);
 	}
 
 	@RequestMapping("/usr/article/getArticles")
