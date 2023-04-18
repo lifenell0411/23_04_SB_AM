b45d543a2fec2c6b9b9bbea02cff6aa4c6d8d383
lifenell0411 <lifenell0411@gmail.com>
2023-04-18 17:54:31 +0900

    게시물 테이블 구조 변경, memberId 추가, doWrite시 로그인 체크, 작성자 정보 저장

--- src/main/java/com/KoreaIT/bjw/demo/controller/UsrArticleController.java
+++ src/main/java/com/KoreaIT/bjw/demo/controller/UsrArticleController.java
@@ -2,6 +2,8 @@ package com.KoreaIT.bjw.demo.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,39 +21,48 @@ public class UsrArticleController {
 	private ArticleService articleService;
 
 	// 액션메서드
-	
-	//////////// 수정
 	@RequestMapping("/usr/article/doModify")
 	@ResponseBody
 	public ResultData<Integer> doModify(int id, String title, String body) {
 		Article article = articleService.getArticle(id);
 		if (article == null) {
-			return ResultData.from("F-1", Ut.f("%d번 글은 존재하지 않습니다", id),id);
-
+			return ResultData.from("F-1", Ut.f("%d번 글은 존재하지 않습니다", id), id);
 		}
 
 		articleService.modifyArticle(id, title, body);
 
-		return ResultData.from("S-1", Ut.f("%d번 글을 수정했습니다", id), id);
+		return ResultData.from("S-1", Ut.f("%d번 글을 수정 했습니다", id), id);
 	}
 
-	////////삭제
 	@RequestMapping("/usr/article/doDelete")
 	@ResponseBody
 	public ResultData<Integer> doDelete(int id) {
 		Article article = articleService.getArticle(id);
 		if (article == null) {
-			return ResultData.from("F-1", Ut.f("%d번 글은 존재하지 않습니다",id));
+			return ResultData.from("F-1", Ut.f("%d번 글은 존재하지 않습니다", id), id);
 		}
 
 		articleService.deleteArticle(id);
 
-		return ResultData.from("S-1", Ut.f("%d번 글을 삭제했습니다",id));
+		return ResultData.from("S-1", Ut.f("%d번 글을 삭제 했습니다", id), id);
 	}
 
 	@RequestMapping("/usr/article/doWrite")
 	@ResponseBody
-	public ResultData<Article> doWrite(String title, String body) {
+	public ResultData<Article> doWrite(HttpSession httpSession, String title, String body) {
+
+		boolean isLogined = false;
+		int loginedMemberId = 0;
+
+		if (httpSession.getAttribute("loginedMemberId") != null) {
+			isLogined = true;
+			loginedMemberId = (int) httpSession.getAttribute("loginedMemberId");
+		}
+
+		if (isLogined == false) {
+			return ResultData.from("F-A", "로그인 후 이용해주세요");
+		}
+
 		if (Ut.empty(title)) {
 			return ResultData.from("F-1", "제목을 입력해주세요");
 		}
@@ -59,13 +70,12 @@ public class UsrArticleController {
 			return ResultData.from("F-2", "내용을 입력해주세요");
 		}
 
-		ResultData writeArticleRd = articleService.writeArticle(title, body);
+		ResultData<Integer> writeArticleRd = articleService.writeArticle(loginedMemberId, title, body);
 
 		int id = (int) writeArticleRd.getData1();
 
 		Article article = articleService.getArticle(id);
 
-	
 		return ResultData.newData(writeArticleRd, article);
 	}
 
