3e476e229b893a38959547376592539d2ffd4d11
lifenell0411 <lifenell0411@gmail.com>
2023-04-20 16:41:17 +0900

    게시물 삭제 시 뒤로가기 또는 지정된 경로로 이동

--- src/main/java/com/KoreaIT/bjw/demo/util/Ut.java
+++ src/main/java/com/KoreaIT/bjw/demo/util/Ut.java
@@ -1,5 +1,4 @@
 package com.KoreaIT.bjw.demo.util;
-
 public class Ut {
 
 	public static boolean empty(Object obj) {
@@ -19,6 +18,43 @@ public class Ut {
 
 	public static String f(String format, Object... args) {
 		return String.format(format, args);
+	}
+
+	public static String jsHitoryBack(String resultCode, String msg) {
+
+		if (msg == null) {
+			msg = "";
+		}
+
+		return Ut.f("""
+				<script>
+					const msg = '%s'.trim();
+					if ( msg.length > 0 ){
+						alert(msg);
+					}
+					history.back();
+				</script>
+				""", msg);
+	}
+
+	public static String jsReplace(String msg, String uri) {
+		if (msg == null) {
+			msg = "";
+		}
+		if (uri == null) {
+			uri = "/";
+		}
+
+		return Ut.f("""
+					<script>
+					const msg = '%s'.trim();
+					if ( msg.length > 0 ){
+						alert(msg);
+					}
+					location.replace('%s');
+				</script>
+				""", msg, uri);
+
 	}
 
 }
