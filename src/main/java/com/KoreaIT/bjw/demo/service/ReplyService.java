5af0f8b8bd3b847a2075cbf35eede0c78034f0f0
lifenell0411 <lifenell0411@gmail.com>
2023-05-02 16:49:14 +0900

    댓글 작성 구현

--- src/main/java/com/KoreaIT/bjw/demo/service/ReplyService.java
+++ src/main/java/com/KoreaIT/bjw/demo/service/ReplyService.java
@@ -1,9 +1,29 @@
 package com.KoreaIT.bjw.demo.service;
 
 
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.KoreaIT.bjw.demo.repository.ReplyRepository;
+import com.KoreaIT.bjw.demo.util.Ut;
+import com.KoreaIT.bjw.demo.vo.ResultData;
 
 @Service
 public class ReplyService {
- 
+
+	@Autowired
+	private ReplyRepository replyRepository;
+
+	public ReplyService(ReplyRepository replyRepository) {
+		this.replyRepository = replyRepository;
+	}
+
+	public ResultData<Integer> writeReply(int actorId, String relTypeCode, int relId, String body) {
+		replyRepository.writeReply(actorId, relTypeCode, relId, body);
+
+		int id = replyRepository.getLastInsertId();
+
+		return ResultData.from("S-1", Ut.f("%d번 댓글이 생성되었습니다", id), "id", id);
+	}
 }
