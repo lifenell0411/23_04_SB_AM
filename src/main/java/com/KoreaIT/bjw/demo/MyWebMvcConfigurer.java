a4008b5caf30f5397e18dacb4e07c57676fa9eb4
lifenell0411 <lifenell0411@gmail.com>
2023-05-09 15:25:55 +0900

    회원가입시 Ajax 로 로그인 아이디 중복체크

--- src/main/java/com/KoreaIT/bjw/demo/MyWebMvcConfigurer.java
+++ src/main/java/com/KoreaIT/bjw/demo/MyWebMvcConfigurer.java
@@ -64,6 +64,7 @@ public class MyWebMvcConfigurer implements WebMvcConfigurer {
 		ir = registry.addInterceptor(needLogoutInterceptor);
 		ir.addPathPatterns("/usr/member/login");
 		ir.addPathPatterns("/usr/member/doLogin");
+		ir.addPathPatterns("/usr/member/getLoginIdDup");
 		ir.addPathPatterns("/usr/member/join");
 		ir.addPathPatterns("/usr/member/doJoin");
 		ir.addPathPatterns("/usr/member/findLoginId");
@@ -73,3 +74,4 @@ public class MyWebMvcConfigurer implements WebMvcConfigurer {
 	}
 
 }
+ 
